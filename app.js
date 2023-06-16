5f528cea19e9f6598c9701d8bad7b337595bf469
elamartin <martin2011522@ceu.edu.ph>
2023-06-16 22:20:31 +0800

    app is changed

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 const db = require("./API/models/connection_db")
 db.connectDatabase()
 
-const prodRouter = require('./API/routers/product_router')
+const product_router = require('./API/routers/product_router')
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}))
@@ -18,13 +18,87 @@ app.use((req, res, next)=>{
 
     if (req.method === 'OPTIONS'){
         res.header("Access-Control-Allow-Methods", "*")
-        return res.status(200).jjjson({})
+        return res.status(200).json({})
     }
 
     next()
 })
 
-app.use('/products', prodRouter)
+app.use('/products', product_router)
+
+app.use((req, res, next)=>{
+    const error = new Error('Not Found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500)
+    res.json({
+        error:{
+            message: error.message
+        }
+    })
+})
+
+////////////// EMPLOYEES
+
+const employees_router = require('./API/routers/employees_router')
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.use((req, res, next)=>{
+    res.header("Acccess-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+
+    if (req.method === 'OPTIONS'){
+        res.header("Access-Control-Allow-Methods", "*")
+        return res.status(200).json({})
+    }
+
+    next()
+})
+
+app.use('/employees', employees_router)
+
+app.use((req, res, next)=>{
+    const error = new Error('Not Found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500)
+    res.json({
+        error:{
+            message: error.message
+        }
+    })
+})
+
+///////////// FOR SUPPLIER
+
+const supplier_router = require('./API/routers/supplier_router')
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.use((req, res, next)=>{
+    res.header("Acccess-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+
+    if (req.method === 'OPTIONS'){
+        res.header("Access-Control-Allow-Methods", "*")
+        return res.status(200).json({})
+    }
+
+    next()
+})
+
+app.use('/supplier', supplier_router)
 
 app.use((req, res, next)=>{
     const error = new Error('Not Found')
